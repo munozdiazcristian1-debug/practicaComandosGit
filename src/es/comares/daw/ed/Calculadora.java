2f91060c07a491c5077512a4c4cab9e945fbfcf9
Cristian Muñoz Díaz <munozdiazcristian1@gmail.com>
2025-11-01 12:47:55 +0100

    Resta

--- src/es/comares/daw/ed/Calculadora.java
+++ src/es/comares/daw/ed/Calculadora.java
@@ -4,8 +4,8 @@ public class Calculadora {
 
 	public static void main(String[] args) {
 		System.out.println("Inicio");
-		System.out.println("Suma");
-		
+		System.out.println("1. Suma");
+		System.out.println("2. Resta");
 	}
 
 }
